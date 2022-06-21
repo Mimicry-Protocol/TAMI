1abfb77fd3ce1e2bb8000fd02dd18b1fdee77d5f
Daniel Cortes <danielgcortes@gmail.com>
2022-06-21 16:36:59 -0400

    Export utils

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export * from './lib/tami/tami';
 export { dynamicTami } from './lib/tami/dynamicTami';
+export { filterValidTransactions } from './lib/utils/filterValidTransactions';
+export { sortTransactions } from './lib/utils/sortTransactions';
+
+export type {
+  Transaction,
+  TransactionMap,
+  IndexValueHistoryItem,
+} from './lib/types';
